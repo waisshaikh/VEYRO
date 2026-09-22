a8721a826950c410906ac021d5edb13e5cfe08b4
waisshaikh <waisshaikh394@gmail.com>
2026-09-23 02:25:41 +0530

    fix: correct relative import paths for config/api.js in all service files

--- frontend/src/features/product/services/product.api.js
+++ frontend/src/features/product/services/product.api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_BASE_URL } from "../../config/api.js";
+import { API_BASE_URL } from "../../../config/api.js";
 
 const productApiInstance = axios.create({
     baseURL: `${API_BASE_URL}/products`,
