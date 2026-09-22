3af8676013f03d06ce6c76a79b23462b1ac9b1a8
waisshaikh <waisshaikh394@gmail.com>
2026-09-22 21:24:35 +0530

    feat: add Vercel configuration file and update API target in Vite config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000" || "https://veyro-r5ci.onrender.com/",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       }
